67c388d7a48f2847ef20b9503057eecca3085016
enrico <enricogregorio5@gmail.com>
2025-07-16 20:10:55 -0300

    Alterei a ordem das mudanças
    Ao trocar o tema do site, as frases não trocavam de cor.
    
    Verifiquei que ao colocar a messagem vinda do app.py por último no javaScript funcionava.

--- static/js/script.js
+++ static/js/script.js
@@ -4,26 +4,27 @@ function showLoadingMessage() {
 
 function changeBgColor(color) {
     let colorMode = color;
-    const COR0 = 'var(--cor0)';
-    const COR1 = 'var(--cor1)';
+    const COR_PRETA = 'var(--cor0)';
+    const COR_CLARA = 'var(--cor1)';
 
     if (colorMode === 'dark') {
         document.getElementById('dark-mode').style.display = 'none'
         document.getElementById('light-mode').style.display = 'block'
 
-        document.getElementById('page').style.backgroundColor = COR1
-        document.querySelector("p").style.color = COR0
-        document.getElementById("message").style.color = COR0
-        document.querySelector("h1").style.color = COR0
-        document.querySelector("label").style.color = COR0
+        document.getElementById('page').style.backgroundColor = COR_CLARA
+        document.querySelector("p").style.color = COR_PRETA
+        document.querySelector("h1").style.color = COR_PRETA
+        document.querySelector("label").style.color = COR_PRETA
+        document.getElementById("message").style.color = COR_PRETA
     } else {
         document.getElementById('dark-mode').style.display = 'block'
         document.getElementById('light-mode').style.display = 'none'
 
-        document.getElementById('page').style.backgroundColor = COR0
-        document.querySelector("p").style.color = COR1
-        document.getElementById("message").style.color = COR1
-        document.querySelector("h1").style.color = COR1
-        document.querySelector("label").style.color = COR1
+        document.getElementById('page').style.backgroundColor = COR_PRETA
+        document.querySelector("p").style.color = COR_CLARA
+        document.querySelector("h1").style.color = COR_CLARA
+        document.querySelector("label").style.color = COR_CLARA
+        document.getElementById("message").style.color = COR_CLARA
+
     }
 }
